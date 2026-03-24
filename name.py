951223b1cc1f3ed2d8db1c80afca94e2f2edfbaf
spp1997 <suprhashi188797s@gmail.com>
2026-03-24 15:13:45 +0530

    basic display program

---
+++ name.py
@@ -0,0 +1,13 @@
+#Stores a string in a variable
+first_name="Ada"
+last_name="lovelace"
+
+#Adds strings together and stores as one string in a variable
+full_name=first_name+ " "+ last_name
+
+#title() makes first letter of each word upper case.
+#upper() makes every letter upper case.
+#lower() makes every letter lower case.
+
+message="Hello, " +full_name.title()+ "!"
+print(message)
