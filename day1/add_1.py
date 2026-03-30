275c416becc63ff85539a6ba24bf4ac22d7f9d8a
Sunita <suprhashi188797s@gmail.com>
2026-03-30 21:10:45 +0530

    Addition of 2 numbers

---
+++ day1/add_1.py
@@ -0,0 +1,20 @@
+'''Input: num1 = 5, num2 = 3
+Output: 8
+
+Input: num1 = 13, num2 = 6
+Output: 19'''
+
+# Python3 program to add two numbers
+
+num1 = 15
+num2 = 12
+num3=40
+num4=20
+
+# Adding two nos
+sum = num1 + num2
+sum1=num3+num4
+
+# printing values
+print("Sum of {0} and {1} is {2}" .format(num1, num2, sum))
+print("Sum of {0} and {1} is {2}" .format(num3,num4,sum1))
