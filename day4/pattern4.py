496b83ad1abe3fd571ff414417d7890886134918
Sunita <suprhashi188797s@gmail.com>
2026-04-14 22:49:06 +0530

    inverted number traiangle

---
+++ day4/pattern4.py
@@ -0,0 +1,7 @@
+# Inverted Number Right-Angled Triangle Pattern in Python
+
+rows = int(input("Enter the row size for the pattern: "))
+for i in range(rows, 0, -1):  # Outer loop for rows in reverse
+    for j in range(1, i + 1):  # Inner loop for columns
+        print(j, end=" ")  # Print numbers
+    print()
