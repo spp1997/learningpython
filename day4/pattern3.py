a2d4d116fa390a6f7438bce8094c7bc5ef760bc4
Sunita <suprhashi188797s@gmail.com>
2026-04-14 18:41:13 +0530

    Right Angled triangle for displaying numbers

---
+++ day4/pattern3.py
@@ -0,0 +1,8 @@
+#Number Right-Angled Triangle Pattern in Python
+
+
+rows = int(input("Enter the row size for the pattern:"))
+for i in range(1, rows + 1):  # Outer loop for rows
+    for j in range(1, i + 1):  # Inner loop for columns
+        print(j, end=" ")  # Print numbers
+    print()
