4ef081567b27e87a32b22ffc215b69c69778c568
spp1997 <suprhashi188797s@gmail.com>
2026-07-05 09:43:40 +0000

    Usage of multiple variables

---
+++ day5/varmultiple.py
@@ -0,0 +1,19 @@
+#Many values to multiple variables
+
+x,y,z= "Orange","Banana","Cherry"
+print(x)
+print(y)
+print(z)
+
+#One value to multiple variables
+x=y=z="Orange"
+print(x)
+print(y)
+print(z)
+
+#Unpack a list
+fruits=["apple","banana","Cherry"]
+x,y,z=fruits
+print(x)
+print(y)
+print(z)
