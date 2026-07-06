b1cf0fc407466e0e8e7c653e50f061a8acc244db
spp1997 <suprhashi188797s@gmail.com>
2026-07-06 15:11:21 +0000

    Usage of numbers

---
+++ day5/numbers.py
@@ -0,0 +1,37 @@
+x=1
+y=2.8
+z=1j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+#Int numbers example
+
+x=123
+y=6789644323456789
+z=-89765
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Float numbers example
+
+x=1.10
+y=1.0
+z=-35.59
+print(type(x))
+print(type(y))
+print(type(z))
+
+#floats can be scientific numbers with an "e" to indicate the power of 10
+
+x=35e3
+y=12E4
+z=-87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
