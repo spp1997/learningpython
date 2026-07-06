28e73a58038db65f966d0f25b411fd02cf896aad
spp1997 <suprhashi188797s@gmail.com>
2026-07-06 15:36:15 +0000

    Use of complex number ,conversion,random numbers

---
+++ day5/number1.py
@@ -0,0 +1,40 @@
+#complex number
+
+x=3+5j
+y=5j
+z=-5j
+
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+#type Conversion
+
+x=1
+y=2.8
+z=1j
+
+#convert from int to float
+a=float(x)
+
+#convert from float to int
+b=int(y)
+
+#Convert from int to complex
+c=complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+#Random Number
+
+import random 
+print(random.randrange(1,10))
+
