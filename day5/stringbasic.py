c48ed2945c27db4ea0b20fb3538be8d9066fbc60
spp1997 <suprhashi188797s@gmail.com>
2026-07-07 06:36:32 +0000

    string Basics

---
+++ day5/stringbasic.py
@@ -0,0 +1,35 @@
+# Strings: Styrings in python are surrounded by either single quotation marks or double quotation marks.
+
+print("Hello")
+print('Hello')
+
+#Quotes inside quotes
+
+print("It's alright")
+print("he is called 'Johnny'")
+print('He is called "Johnny"')
+
+
+#Assign String to a variable
+a= "Hello"
+print(a)
+
+#Multiline strings
+#You can assign a multiline string to a variable by using three quotes
+
+a= """ I am Sunita here
+Wanted to learn devops
+so learning basics required linux fundamenatls, bash scripting
+and git & github """
+
+print(a)
+
+# Or use three single quotes
+a =''' I am sunita here
+i am learning mlops
+so python is mandorty for that'''
+
+print(a)
+
+
+
