4b95dc1c7dea247775321ddbfa1c00c121d8b75d
spp1997 <suprhashi188797s@gmail.com>
2026-07-13 09:54:01 +0000

    Booleans

---
+++ day6/boolean1.py
@@ -0,0 +1,23 @@
+#Print a message based on whether the condtion is True or False
+
+a = 200
+b = 33
+
+if b > a:
+      print("b is greater than a")
+else:
+      print("b is not greater than a")
+
+#the bool() function allows you to evaluate any value and give you True or False in return 
+
+print(bool("Hello"))
+print(bool(15))
+
+x="Hello"
+y=15
+
+print(bool(x))
+print(bool(y))
+
+
+
